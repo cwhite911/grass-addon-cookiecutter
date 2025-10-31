1c48c6d6716c549dc47d387904cd39d175d43a0d
cwhite911 <smortopahri@gmail.com>
2025-10-30 21:26:34 -0400

    Removed trailing whitespace

--- {{ cookiecutter.tool }}/testsuite/test_{{  cookiecutter.__tool}}.py
+++ {{ cookiecutter.tool }}/testsuite/test_{{  cookiecutter.__tool}}.py
@@ -7,4 +7,4 @@ from grass.tools import Tools
 def test_output_exists(session):
     tools = Tools(session=session, consistent_return_value=False)
     output = tools.{{ cookiecutter.__tool }}(input="data", output=np.array)
-    assert np.any(np.isnan(output)) 
+    assert np.any(np.isnan(output))
